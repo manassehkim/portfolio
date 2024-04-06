d21409977d59167b8c59d73cc1d044b5dccb7632
Manassehkim <mainasseh@gmail.com>
2024-04-06 10:49:26 +0300

    portfolio

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const projects = [
+  {
+    title: "Phonebook App",
+    subtitle: "Managing your contacts",
+    description:
+      "Below is a project created using python that is used to add your contacts and store them in the cloud. This reduces the hustle of looking for contacts again.",
+    image: "./phone.jpg",
+    link: "",
+  },
+  {
+    title: "Stock Management",
+    subtitle: "Management of your stocks in the store",
+    description:
+      "This is a blockchain and react app that is used to manage your stocks. You have immutable storage of your stocks data that cannot be changed or manupulated",
+    image: "./stock.png",
+    link: "",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "It has been a great pleasure working with you and if you get another chance we would welcome you back.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Kennedy Syuki",
+    company: "Garth Day Year Auctioneers",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "Python",
+  "Blockchain",
+];
